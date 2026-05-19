132a1730fe58a255fc65c0e038dd2dfb65f42c83
Andrei Costasco <emailul_tau@gmail.com>
2026-05-19 12:12:52 +0300

    feat: CategoryManagementView, UserManagementView, AuditLogView, ReportsView redesign

---
+++ Views/AuditLogView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace PharmaDesk.Views
+{
+    public partial class AuditLogView : Page
+    {
+        public AuditLogView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Views/ReportsView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace PharmaDesk.Views
+{
+    public partial class ReportsView : Page
+    {
+        public ReportsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
