600ac14c10aff1ba8cd2e86a213aa30aa134156d
Andrei Costasco <emailul_tau@gmail.com>
2026-05-19 12:05:54 +0300

    feat: AdminOrdersView (filter tabs + sortable grid) and ProductManagementView (split layout + drag-drop upload)

---
+++ Views/AdminOrdersView.xaml.cs
@@ -0,0 +1,6 @@
+namespace PharmaDesk.Views;
+
+public partial class AdminOrdersView : System.Windows.Controls.Page
+{
+    public AdminOrdersView() => InitializeComponent();
+}
---
+++ Views/ProductManagementView.xaml.cs
@@ -0,0 +1,42 @@
+using System.Windows;
+using System.Windows.Media;
+
+namespace PharmaDesk.Views;
+
+public partial class ProductManagementView : System.Windows.Controls.Page
+{
+    public ProductManagementView() => InitializeComponent();
+
+    private void ImageDropZone_DragOver(object sender, DragEventArgs e)
+    {
+        if (e.Data.GetDataPresent(DataFormats.FileDrop))
+        {
+            e.Effects = DragDropEffects.Copy;
+            ImageDropZone.BorderBrush = new SolidColorBrush(Color.FromRgb(0xFC, 0xA3, 0x11));
+        }
+        else
+        {
+            e.Effects = DragDropEffects.None;
+        }
+        e.Handled = true;
+    }
+
+    private void ImageDropZone_DragLeave(object sender, DragEventArgs e)
+    {
+        // Reset to theme border — bind back via resource
+        ImageDropZone.ClearValue(System.Windows.Controls.Border.BorderBrushProperty);
+    }
+
+    private void ImageDropZone_Drop(object sender, DragEventArgs e)
+    {
+        if (e.Data.GetDataPresent(DataFormats.FileDrop))
+        {
+            var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files.Length > 0 && DataContext is ViewModels.ProductManagementViewModel vm)
+            {
+                vm.SetImagePath(files[0]);
+            }
+        }
+        ImageDropZone.ClearValue(System.Windows.Controls.Border.BorderBrushProperty);
+    }
+}
